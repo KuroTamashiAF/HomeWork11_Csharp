fe0a6a8a33ab372c1e7df70261c734e721364092
Alex <chugun777akk@mail.ru>
2022-04-22 15:25:28 +0700

    task D is done

---
+++ HW11Task_D_/Program.cs
@@ -0,0 +1,32 @@
+﻿// D. Наименьшее число
+
+using System;
+using System.Linq;
+namespace Myprogramm
+{
+    class Programm
+    {
+        static int MinElement(int[] arr)
+        {
+            int Min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < Min)
+                {
+                    Min = arr[i];
+                }
+            }
+            return Min;
+        }
+
+
+        private static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] Array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int Min = MinElement(Array);
+            Console.WriteLine(Min);
+
+        }
+    }
+}

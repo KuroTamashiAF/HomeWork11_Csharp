7e2f3e34b7a0d6dc5c1a8c2948326e5affff3d3e
Alex <chugun777akk@mail.ru>
2022-04-22 15:30:01 +0700

    Task E is done

---
+++ HW11Task_E_/Program.cs
@@ -0,0 +1,32 @@
+﻿// E. Наибольшее число
+
+using System;
+using System.Linq;
+namespace Myprogramm
+{
+    class Programm
+    {
+        static int MinElement(int[] arr)
+        {
+            int Max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > Max)
+                {
+                    Max = arr[i];
+                }
+            }
+            return Max;
+        }
+
+
+        private static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] Array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int Max = MinElement(Array);
+            Console.WriteLine(Max);
+
+        }
+    }
+}

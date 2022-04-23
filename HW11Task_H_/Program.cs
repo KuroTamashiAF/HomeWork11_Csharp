387f23f34039cbd213c4f5924ee7b8e91ef26be6
Alex <chugun777akk@mail.ru>
2022-04-23 12:17:54 +0700

    task h is done

---
+++ HW11Task_H_/Program.cs
@@ -0,0 +1,62 @@
+﻿// D. Ваня и книги 
+
+
+using System;
+using System.Linq;
+namespace Myprogramm
+{
+    class Programm
+    {
+        static void FiilArray(int[] arr)
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
+        }
+        // static void PrintArray(int[] arr)
+        // {
+        //     for (int i = 0; i < arr.Length; i++)
+        //     {
+        //         Console.Write($"{arr[i]} ");
+        //     }
+        // }
+        static int NumberOfDigits(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int number = arr[i];
+                while (number > 0)
+                {
+                    number /= 10;
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[N];
+            FiilArray(array);
+            //PrintArray(array);
+            int CountOfDigit = NumberOfDigits(array);
+            Console.WriteLine(CountOfDigit);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+

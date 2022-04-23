d6488dbbe93ad3b2db4062bb7874730987f08d74
Alex <chugun777akk@mail.ru>
2022-04-23 11:11:23 +0700

    task g is done

---
+++ HW11Task_G_/Program.cs
@@ -0,0 +1,44 @@
+﻿// G. Линейный поиск
+
+
+using System;
+using System.Linq;
+namespace Myprogramm
+{
+    class Programm
+    {
+        static int FindElement(int[] arr, int K)
+        {
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == K)
+                {
+                    return (i + 1);
+                    
+                }
+            }
+            return -1;
+        }
+        // static void PrintArray(int[] arr)
+        // {
+        //     for (int i = 0; i < arr.Length; i++)
+        //     {
+        //         Console.Write($"{arr[i]} ");
+        //     }
+        // }
+
+
+
+        private static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] Array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int Key = Convert.ToInt32(Console.ReadLine());
+            //PrintArray(Array);
+            Console.WriteLine();
+            int IndexKey = FindElement(Array, Key);
+            Console.WriteLine(IndexKey);
+        }
+    }
+}
